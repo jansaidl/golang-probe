c79968c2d6ee66118d7785165f1bfa04ed872c03
Jan Saidl <saidl@vshosting.cz>
2021-09-09 08:57:07 +0200

    add server

--- server/main.go
+++ server/main.go
@@ -12,14 +12,24 @@ import (
 
 func main() {
 
-	var i uint64
+	var i, j uint64
 
 	fmt.Println("starting")
 	go func() {
 		http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			atomic.AddUint64(&i, 1)
 			w.Header().Add("Content-type", "text/plain")
-			fmt.Fprintf(w, "Hello %d ;o)\n\n", i)
+			fmt.Fprintf(w, "Hello %d ;o)\n\n", i, j)
+			for _, e := range os.Environ() {
+				fmt.Fprintf(w, "%s\n", e)
+			}
+		}))
+	}()
+	go func() {
+		http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			atomic.AddUint64(&j, 1)
+			w.Header().Add("Content-type", "text/plain")
+			fmt.Fprintf(w, "Hello %d / %d;o)\n\n", i, j)
 			for _, e := range os.Environ() {
 				fmt.Fprintf(w, "%s\n", e)
 			}
