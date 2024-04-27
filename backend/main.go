f425dba891c175c7fe784e691dcff402a1e150b5
Jan Saidl <saidl@vshosting.cz>
2024-04-27 22:34:44 +0200

    WIP

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import "net/http"
 
 func main() {
 	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
 		w.Write([]byte("Hello, World!"))
 	}))
 }
